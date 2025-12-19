e63c7551933c357500a79b0b16e4a267fdd2157d
Awais. <90985091+awsmlk@users.noreply.github.com>
2025-12-19 10:07:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const products = [
         name: "Essential Denim Jacket",
         category: "essentials",
         price: 120,
-        image: "https://unsplash.com/photos/man-leaning-on-tree-KKZmUQjTO2Ehttps://images.unsplash.com/photo-1537465978529-d23b17165b3b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        image: "https://images.unsplash.com/photo-1537465978529-d23b17165b3b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         description: "Classic denim jacket with a modern twist. Durable, stylish, and versatile.",
         isBestSeller: false
     },
